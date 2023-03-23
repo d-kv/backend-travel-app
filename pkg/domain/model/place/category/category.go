34db9c9df4d813bd8b2610eb13ade0ba12357363
Ruslan Sorokin <strawberryladder@gmail.com>
2023-03-23 17:45:19 +0300

    feat(category): add `Builder` class

--- pkg/domain/model/place/category/category.go
+++ pkg/domain/model/place/category/category.go
@@ -18,3 +18,61 @@ type Category struct {
 	Food          Food
 	Hospitality   Hospitality
 }
+
+type builderI interface {
+	Culture(v Culture) builderI
+	Entertainment(v Entertainment) builderI
+	Food(v Food) builderI
+	Hospitality(v Hospitality) builderI
+
+	Build() Category
+}
+
+type Builder struct {
+	category ECategory
+
+	culture       Culture
+	enterteinment Entertainment
+	food          Food
+	hospitality   Hospitality
+}
+
+var _ builderI = Builder{}
+
+func (b Builder) Culture(v Culture) builderI {
+	b.category = C_CULTURE
+	b.culture = v
+	return b
+}
+
+func (b Builder) Entertainment(v Entertainment) builderI {
+	b.category = C_ENTERTAINMENT
+	b.enterteinment = v
+	return b
+}
+
+func (b Builder) Food(v Food) builderI {
+	b.category = C_FOOD
+	b.food = v
+	return b
+}
+
+func (b Builder) Hospitality(v Hospitality) builderI {
+	b.category = C_HOSPITALITY
+	b.hospitality = v
+	return b
+}
+
+func (b Builder) Build() Category {
+	return Category{
+		Category:      b.category,
+		Culture:       b.culture,
+		Enterteinment: b.enterteinment,
+		Food:          b.food,
+		Hospitality:   b.hospitality,
+	}
+}
+
+func NewBuilder() builderI {
+	return Builder{}
+}
