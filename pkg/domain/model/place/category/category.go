661eb1d83163101ce8ab99ce2908ab9c060b9028
Ruslan Sorokin <strawberryladder@gmail.com>
2023-03-23 14:33:54 +0300

    refactor(category): fix typos

--- pkg/domain/model/place/category/category.go
+++ pkg/domain/model/place/category/category.go
@@ -5,7 +5,7 @@ type ECategory int32
 //nolint:revive, stylecheck // Using SNAKE_CASE for enums
 const (
 	C_CULTURE ECategory = iota + 1
-	C_ENTARTAINMENT
+	C_ENTERTAINMENT
 	C_FOOD
 	C_HOSPITALITY
 )
@@ -14,7 +14,7 @@ type Category struct {
 	Category ECategory
 
 	Culture       Culture
-	Enterteinment Enterteinment
+	Enterteinment Entertainment
 	Food          Food
 	Hospitality   Hospitality
 }
