ab788126a76ee018ccad89df86c630b8b6b8313b
Ruslan Sorokin <strawberryladder@gmail.com>
2023-03-25 11:21:34 +0300

    feat: add search query

---
+++ pkg/domain/model/query/search.go
@@ -0,0 +1,10 @@
+package query
+
+import (
+	"github.com/d-kv/backend-travel-app/pkg/domain/model/place/category"
+)
+
+type Search struct {
+	Categories []category.Category
+	Distance   Distance
+}
