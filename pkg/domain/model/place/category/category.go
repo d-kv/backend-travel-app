294d9d771332e7016a1eab00d0a2fa1b4bc399a1
Ruslan Sorokin <strawberryladder@gmail.com>
2023-03-28 19:26:44 +0300

    docs(category): add docstrings

--- pkg/domain/model/place/category/category.go
+++ pkg/domain/model/place/category/category.go
@@ -10,64 +10,75 @@ const (
 	C_HOSPITALITY
 )
 
+// Category indicates category of a place & sub_category for each category.
+//
+// Use getters to check category.
 type Category struct {
 	Category ECategory `bson:"category"`
 
 	Value int32 `bson:"sub_category"`
 }
 
+// Culture returns Culture category if specified, otherwise returns 0.
 func (c Category) Culture() Culture {
-	if c.category != C_CULTURE {
+	if c.Category != C_CULTURE {
 		return 0
 	}
-	return Culture(c.value)
+	return Culture(c.Value)
 }
 
+// Entertainment returns Entertainment category if specified, otherwise returns 0.
 func (c Category) Entertainment() Entertainment {
-	if c.category != C_ENTERTAINMENT {
+	if c.Category != C_ENTERTAINMENT {
 		return 0
 	}
-	return Entertainment(c.value)
+	return Entertainment(c.Value)
 }
 
+// Food returns Food category if specified, otherwise returns 0.
 func (c Category) Food() Food {
-	if c.category != C_FOOD {
+	if c.Category != C_FOOD {
 		return 0
 	}
-	return Food(c.value)
+	return Food(c.Value)
 }
 
+// Hospitality returns Hospitality category if specified, otherwise returns 0.
 func (c Category) Hospitality() Hospitality {
-	if c.category != C_HOSPITALITY {
+	if c.Category != C_HOSPITALITY {
 		return 0
 	}
-	return Hospitality(c.value)
+	return Hospitality(c.Value)
 }
 
+// NewCulture is a default ctor for Culture category.
 func NewCulture(v Culture) Category {
 	return Category{
-		category: C_CULTURE,
-		value:    int32(v),
+		Category: C_CULTURE,
+		Value:    int32(v),
 	}
 }
 
+// NewEntertainment is a default ctor for Entertainment category.
 func NewEntertainment(v Entertainment) Category {
 	return Category{
-		category: C_ENTERTAINMENT,
-		value:    int32(v),
+		Category: C_ENTERTAINMENT,
+		Value:    int32(v),
 	}
 }
 
+// NewFood is a default ctor for Food category.
 func NewFood(v Food) Category {
 	return Category{
-		category: C_FOOD,
-		value:    int32(v),
+		Category: C_FOOD,
+		Value:    int32(v),
 	}
 }
 
+// NewHospitality is a default ctor for Hospitality category.
 func NewHospitality(v Hospitality) Category {
 	return Category{
-		category: C_HOSPITALITY,
-		value:    int32(v),
+		Category: C_HOSPITALITY,
+		Value:    int32(v),
 	}
 }
