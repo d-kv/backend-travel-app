91e9f0db81984736aa45603f9768f13e00b90863
Ruslan Sorokin <strawberryladder@gmail.com>
2023-04-02 18:17:17 +0300

    refactor(model:Version): add `ParseVersionFromString` func

--- pkg/domain/model/util/version.go
+++ pkg/domain/model/util/version.go
@@ -1,17 +1,40 @@
 package util
 
+import (
+	"strconv"
+	"strings"
+)
+
+const (
+	precision = 6
+	bitSize   = 64
+	base      = 10
+)
+
 type Version struct {
 	// TODO: use standard Major.Minor.Path versioning
 	// TODO: Patch might be a string like rc
-	Patch int32
+	Patch uint64
 }
 
 func (v Version) Less(otherV Version) bool {
 	return v.Patch > otherV.Patch
 }
 
-func NewVersion(patch int32) *Version {
+func NewVersion(patch uint64) *Version {
 	return &Version{
 		Patch: patch,
 	}
+}
+
+// ParseVersionFromString populates Version from a string of the form "<Patch>".
+func ParseVersionFromString(v *Version, rawStr string) error {
+	p, err := strconv.ParseUint(strings.TrimSpace(rawStr), base, bitSize)
+	if err != nil {
+		return err
+	}
+
+	v.Patch = p
+
+	return nil
 }
