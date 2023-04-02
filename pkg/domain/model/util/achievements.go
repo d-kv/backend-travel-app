dd9bcbe5ce849e4dcc5f425e02b9364bb61c8882
Ruslan Sorokin <strawberryladder@gmail.com>
2023-04-02 18:08:25 +0300

    refactor(model:util): add default ctors

--- pkg/domain/model/util/achievements.go
+++ pkg/domain/model/util/achievements.go
@@ -3,3 +3,7 @@ package util
 type Achievements struct {
 	string //nolint:unused // Using dummy way to store achievements
 }
+
+func NewAchievements(ach string) *Achievements {
+	return &Achievements{ach}
+}
--- pkg/domain/model/util/version.go
+++ pkg/domain/model/util/version.go
@@ -2,9 +2,16 @@ package util
 
 type Version struct {
 	// TODO: use standard Major.Minor.Path versioning
+	// TODO: Patch might be a string like rc
 	Patch int32
 }
 
 func (v Version) Less(otherV Version) bool {
 	return v.Patch > otherV.Patch
+}
+
+func NewVersion(patch int32) *Version {
+	return &Version{
+		Patch: patch,
+	}
 }
