520247dea9e1317879bf95c6c660dbd33a4bd183
Ruslan Sorokin <strawberryladder@gmail.com>
2023-04-02 09:33:30 +0300

    feat(model:LatLng): add `ParseFromString` func

--- pkg/domain/model/util/latlng.go
+++ pkg/domain/model/util/latlng.go
@@ -1,13 +1,37 @@
 package util
 
+import (
+	"strconv"
+	"strings"
+)
+
 type LatLng struct {
 	Latitude  float64 `bson:"latitude"`
 	Longitude float64 `bson:"longitude"`
 }
 
-func NewLatLng(lat, lng float64) LatLng {
-	return LatLng{
+func NewLatLng(lat, lng float64) *LatLng {
+	return &LatLng{
 		Latitude:  lat,
 		Longitude: lng,
 	}
+}
+
+// ParseLatLngFromString populates LatLng from a string of the form "<latitude>,<longitude>".
+func ParseLatLngFromString(ll *LatLng, rawStr string) error {
+	parts := strings.Split(rawStr, ",")
+
+	lat, err := strconv.ParseFloat(strings.TrimSpace(parts[0]), 64)
+	if err != nil {
+		return err
+	}
+	ll.Latitude = lat
+
+	lng, err := strconv.ParseFloat(strings.TrimSpace(parts[1]), 64)
+	if err != nil {
+		return err
+	}
+	ll.Longitude = lng
+
+	return nil
 }
