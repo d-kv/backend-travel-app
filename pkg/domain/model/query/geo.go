b1de6f3a13119bbf0ad5c341b3dae14044045679
Ruslan Sorokin <strawberryladder@gmail.com>
2023-04-02 07:32:34 +0300

    refactor(model:query:Geo): rename `SearchAreaCenter` => `Center`

--- pkg/domain/model/query/geo.go
+++ pkg/domain/model/query/geo.go
@@ -3,7 +3,7 @@ package query
 import "github.com/d-kv/backend-travel-app/pkg/domain/model/util"
 
 type Geo struct {
-	SearchAreaCenter util.LatLng
-	Min              int32
-	Max              int32
+	Center util.LatLng
+	Min    int32
+	Max    int32
 }
