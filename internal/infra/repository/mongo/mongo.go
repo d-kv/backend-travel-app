963a532790c9326a5f0d2fc0c4061d1e1858c9e3
Ruslan Sorokin <strawberryladder@gmail.com>
2023-04-03 03:41:17 +0300

    chore(repository:mongo): minor changes in logging

--- internal/infra/repository/mongo/mongo.go
+++ internal/infra/repository/mongo/mongo.go
@@ -14,16 +14,16 @@ const connTimeout = 10
 func NewClient(uri string) (*mongo.Client, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), connTimeout*time.Second)
 
-	log.Println("Connect: attempt to connect to mongoDB")
+	log.Println("NewClient: attempt to connect to mongoDB at:", uri)
 
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 
 	if err != nil {
-		log.Printf("Connect: mongoDB connection error:, %v\n", err)
+		log.Printf("NewClient: mongoDB connection error:, %v\n", err)
 		return nil, err
 	}
 
-	log.Println("Connect: mongoDB successful connection")
+	log.Println("NewClient: mongoDB successful connection")
 	return client, nil
 }
