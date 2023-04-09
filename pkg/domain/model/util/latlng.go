fbd92ff66da424aaa98d81c1a7cb9dc7a0e3f054
Ruslan Sorokin <strawberryladder@gmail.com>
2023-04-09 03:28:41 +0300

    feat(model:LatLng): add ctor for a reversed string

--- pkg/domain/model/util/latlng.go
+++ pkg/domain/model/util/latlng.go
@@ -3,8 +3,14 @@
 package util
 
 import (
+	"errors"
 	"strconv"
 	"strings"
+)
+
+var (
+	ErrUnableToParseLatLng = errors.New("unable to parse LatLng from string")
+	ErrInvalidLatLng       = errors.New("latLng parameter is invalid")
 )
 
 // LatLng stores latitude & longitude.
@@ -23,28 +29,65 @@ func NewLatLng(lat, lng float64) *LatLng {
 
 // NewLatLngFromString creates a new LatLng from a string of the form "<latitude>,<longitude>".
 func NewLatLngFromString(llStr string) (*LatLng, error) {
-	ll := &LatLng{}
-	if err := parseLatLngFromString(ll, llStr); err != nil {
-		return nil, err
+	lat, lng, err := parseLatLngFromString(llStr)
+	if err != nil {
+		return nil, ErrUnableToParseLatLng
+	}
+
+	if lat > 90 || lat < -90 {
+		return nil, ErrInvalidLatLng
+	}
+
+	if lng > 180 || lng < -180 {
+		return nil, ErrInvalidLatLng
+	}
+
+	ll := &LatLng{
+		Latitude:  lat,
+		Longitude: lng,
 	}
 
 	return ll, nil
 }
 
-func parseLatLngFromString(ll *LatLng, rawStr string) error {
+// NewLatLngFromRString creates a new LatLng from a string of the form "<longitude>,<latitude>".
+func NewLatLngFromRString(llStr string) (*LatLng, error) {
+	lng, lat, err := parseLatLngFromString(llStr)
+	if err != nil {
+		return nil, ErrUnableToParseLatLng
+	}
+
+	if lat > 90 || lat < -90 {
+		return nil, ErrInvalidLatLng
+	}
+
+	if lng > 180 || lng < -180 {
+		return nil, ErrInvalidLatLng
+	}
+
+	ll := &LatLng{
+		Latitude:  lat,
+		Longitude: lng,
+	}
+
+	return ll, nil
+}
+
+func parseLatLngFromString(rawStr string) (float64, float64, error) {
 	parts := strings.Split(rawStr, ",")
-
-	lat, err := strconv.ParseFloat(strings.TrimSpace(parts[0]), 64)
-	if err != nil {
-		return err
+	if len(parts) != 2 { //nolint:gomnd // latitude & longitude
+		return 0, 0, ErrUnableToParseLatLng
 	}
-	ll.Latitude = lat
 
-	lng, err := strconv.ParseFloat(strings.TrimSpace(parts[1]), 64)
+	l, err := strconv.ParseFloat(strings.TrimSpace(parts[0]), bitSize)
 	if err != nil {
-		return err
+		return 0, 0, err
 	}
-	ll.Longitude = lng
 
-	return nil
+	ll, err := strconv.ParseFloat(strings.TrimSpace(parts[1]), bitSize)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return l, ll, nil
 }
